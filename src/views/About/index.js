27722d4594097db84ee33256ec06a8874c6dcb0a
zengpeng <1042289178@qq.com>
2019-08-14 00:34:40 +0800

    temp

---
+++ src/views/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import './index.scss'
+
+export default function index() {
+  return (
+    <div className="about">
+      <h2>about 页面</h2>
+      <p>这是我的一个页面</p>
+    </div>
+  )
+}

84fd2a4d96beceea5108b9af6eb0262c03e4699a
zengpeng123 <1042289178@qq.com>
2021-06-08 03:05:33 +0000

    release version

---
+++ precache-manifest.46e8ee76bd902bec8af864bac5d42ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f295ad89111fc8a32d52ef55be428609",
+    "url": "./index.html"
+  },
+  {
+    "revision": "446aeecdf47bd9fe0ac5",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "43f7032ce3604f621311",
+    "url": "./static/js/2.7c4ce4a8.chunk.js"
+  },
+  {
+    "revision": "446aeecdf47bd9fe0ac5",
+    "url": "./static/js/main.96b9d7af.chunk.js"
+  },
+  {
+    "revision": "3a56e66469ac96b3f810",
+    "url": "./static/js/runtime~main.ad5f0069.js"
+  }
+]);
